b494416b608ce119a007729bf3084a35c4dc623c
DeadShotPraveen <shakthi.praveen1010@gmail.com>
2020-09-09 10:22:22 +0530

    initial commit

---
+++ src/components/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtLkOU6wqF717Qy2I_BaQ0DPc1JKSIQck",
+    authDomain: "ecommerce-reactproject.firebaseapp.com",
+    databaseURL: "https://ecommerce-reactproject.firebaseio.com",
+    projectId: "ecommerce-reactproject",
+    storageBucket: "ecommerce-reactproject.appspot.com",
+    messagingSenderId: "612157425488",
+    appId: "1:612157425488:web:ee2e80c1bb7d88b8a84e55",
+    measurementId: "G-5X29ZV25FD"
+  };
